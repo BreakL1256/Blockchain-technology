e177a57eefeb7665e63e9568ddc25a7dca1a053d
Arnas Kičas <arnas.kicas@mif.stud.vu.lt>
2024-09-21 16:16:36 +0300

    Padarytas papildomas kintamasis, kuris pades paivairint maisyma

--- hash.cpp
+++ hash.cpp
@@ -4,18 +4,28 @@
 
 using namespace std;
 
-int main(){
+void hashFunction(){
     string input;
+    int sumOfChars = 0, bitShifterValue = 0;
     getline(cin, input);
-    cout << input << endl;
-    for(int i=0; i<input.length(); i++){
-        bitset<8> hash(input[i]);
-        cout << hash << ":";
-        input[i] = ~input[i];
-        bitset<8> hashed(input[i]);
-        cout << hashed << endl;
+    //cout << input << endl;
+    if(input.length() != 0){
+        for(int i=0; i<input.length(); i++){
+            sumOfChars += input[i];
+        }
+        bitShifterValue = sumOfChars % (input.length() % 256);
+        for(int i=0; i<input.length(); i++){
+            // bitset<8> hash(input[i]);
+            // cout << hash << ":";
+            input[i] = ~input[i];
+            // bitset<8> hashed(input[i]);
+            // cout << hashed << endl;
+        }
+        cout << sumOfChars;
     }
+}
 
-    cout << input;
+int main(){
+    hashFunction();
     return 0;
 }
