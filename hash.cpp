b6d64b0eb6273e40be29807dada1866ba7a30617
Arnas Kičas <arnas.kicas@mif.stud.vu.lt>
2024-09-26 22:57:05 +0300

    Gal veikia trecia versija

--- hash.cpp
+++ hash.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <string>
 #include <bitset>
+#include <sstream>
 #include <cstring>
 #include <cstdlib>  // For std::srand and std::rand
 #include <ctime>    // For std::time
 #include <cstdio>   // For std::fopen and std::fgets
+#include <iomanip>
 
 using namespace std;
 
@@ -89,53 +91,76 @@ void hashFunction(){
 // }
 
 void hashFunction3() {
-    string input, arrayOfInputs[4];
+    string input, arrayOfInputs[4], hashedString;
+    long long hashedNumberArray[4] = {0};
     
+    for(int i = 0; i<4; i++){
+      arrayOfInputs[i] = "0xsetfh654.";
+    }
+
     // Take input from the user
     cout << "String to hash: ";
     getline(std::cin, input);
+
+    if(input.length() == 0) return;
 
     int partLength = input.length() / 4;  
     int remainder = input.length() % 4;   
 
     int currentPos = 0;
 
-    for(int i = 0; i<4; i++){
-        int currentPartLength = partLength + (i < remainder ? 1 : 0);
-        
-        arrayOfInputs[i] = input.substr(currentPos, currentPartLength);
-        
-        currentPos += currentPartLength;
-    }
+    //Split input string
+    if(input.length() >=4 ){
+        for(int i = 0; i<4; i++){
+            int currentPartLength = partLength + (i < remainder ? 1 : 0);
+            
+            arrayOfInputs[i] = input.substr(currentPos, currentPartLength);
+            
+            currentPos += currentPartLength;
+        }
+    } else arrayOfInputs[0] = input;
 
-    unsigned long long hash = 0, hashAlternative = 0; // Initialize hash value
+    unsigned long long hash = 0, hashAlternative = 0; 
 
-    // Process each character in the input string
+    //Hashing the string
     for(int i = 0; i < 4; i++){
         for (char c : arrayOfInputs[i]) {
-            hash ^= static_cast<unsigned char>(c); // XOR the hash with the ASCII value of the character
+            hash ^= static_cast<unsigned char>(c); 
             hashAlternative = hash;
-            hash <<= 2; // Shift left to mix bits
-            hash |= (1LL << 63);
-            hash >>= 10;
-            hash |= (1LL << 63);
+            hash <<= 15; 
             hash ^= hashAlternative;
-            hash |= (1LL << 63);
             hash >>=1;
-            hash |= (1LL << 63);
         }
         arrayOfInputs[i] = to_string(hash);
         hash = 0;
         hashAlternative = 0;
     }
 
-    for(int i=0; i<4;i++){
-        cout << arrayOfInputs[i] << endl;
+    swap(arrayOfInputs[0], arrayOfInputs[2]);
+
+    for(int i=0; i<4; i++){
+        cout << arrayOfInputs[i] << endl; 
     }
+
+    cout << "Hash: ";
+    for(int i=0; i<4;i++){
+        istringstream iss(arrayOfInputs[i]);
+        iss >> hashedNumberArray[i];
+        hash ^= hashedNumberArray[i]; // XOR with current hashed number
+        hash += (hashedNumberArray[i] * 31); // Example multiplication to mix bits
+        hash ^= (hash << 13);           // Shift and XOR for additional mixing
+        hash ^= (hash >> 7);            // More mixing
+        hash |= (1LL << 63);
+        cout << hex << hash;
+       //cmbString += arrayOfInputs[i];
+    }
+
 }
 
 int main(){
     hashFunction3();
-
+    // unsigned long long a = 13839561654909534266ULL, b = 16147665260029935651ULL;
+    // cout << hex << a << endl;
+    // cout << hex << b;
     return 0;
 }
